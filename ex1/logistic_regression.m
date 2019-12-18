54ea085b0374d575c52ecf761925e18bb02ba6e8
Brandon Minor <gallimatrix@gmail.com>
2019-12-18 16:01:38 -0700

    Slow sol to logistic regression

--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -8,12 +8,12 @@ function [f,g] = logistic_regression(theta, X,y)
   %
 
   m=size(X,2);
+  n=size(X,1);
   
   % initialize objective value and gradient.
   f = 0;
   g = zeros(size(theta));
-
-
+  
   %
   % TODO:  Compute the objective function by looping over the dataset and summing
   %        up the objective values for each example.  Store the result in 'f'.
@@ -21,4 +21,13 @@ function [f,g] = logistic_regression(theta, X,y)
   % TODO:  Compute the gradient of the objective by looping over the dataset and summing
   %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
   %
-%%% YOUR CODE HERE %%%
+  
+  for j = 1 : m
+      meas_val = 1 / (1 + exp(-theta' * X(:, j)));
+      cost_one = y(j) * log(meas_val);
+      cost_zero = (1 - y(j)) * log(1 - meas_val);
+      f = f - (cost_one + cost_zero);
+      for i = 1 : n
+          g(i) = g(i) + (X(i, j) * (meas_val - y(j)));
+      end
+  end
